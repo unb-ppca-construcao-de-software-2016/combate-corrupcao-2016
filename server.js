b3cf9d761c02045f15f69838eb8aafc9ed06caa9
felipe-est <fellipe.alves@gmail.com>
2016-08-05 01:21:24 -0300

    Acrescentando credenciais do keen e mongodb

--- server.js
+++ server.js
@@ -5,5 +5,21 @@ server.use(express.static(__dirname));
  
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
-    console.log('server listening on port ' + port);
+    console.log('Servidor rodando na porta' + port);
 });
+
+// Configuração do Keen
+
+var keenIO = require('keen.io');
+
+var keen = keenIO.configure({
+    projectId: process.env['KEEN_PROJECT_ID'],
+    writeKey: process.env['KEEN_WRITE_KEY']
+});
+
+
+// Configurando mongo
+
+var mongodb = require('mongodb');
+var uri = 'mongodb://heroku_7ftmgqmj:99jljcsej6f1mdqh20cgm5pv1v@ds139735.mlab.com:39735/heroku_7ftmgqmj';
+
